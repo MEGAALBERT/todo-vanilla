66afcc80d9a7e388c7d697aebd3634c82f0451f4
Alberto Medellin <albertomedellin85@gmail.com>
2020-10-11 22:08:49 -0700

    added filter option

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo')
 
 //Event Listeners
 
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo)
 
 //Functions
 
@@ -60,4 +62,30 @@ function deleteCheck(event) {
         const todo = item.parentElement; // to grab the parent element
         todo.classList.toggle('completed'); // To change the class for effect
     }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    console.log(todos)
+    todos.forEach(function(todo) {
+        switch(e.target.value) {
+            case 'all':
+                todo.style.display = 'flex';
+                break;
+            case 'completed':
+                if(todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+            case 'uncompleted':
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    })
 }
